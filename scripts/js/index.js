b9afa5b78d4b1fb2acd6088ee8c942e431361cbd
Adrian Palma <adrianpalmasantana@gmail.com>
2025-07-01 21:27:42 -0500

    updates on the 'provinces' array name to states and updated eventlistener that loads the states to the select tag. New event listener for the submit button that stores the data from the form to an array.

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,14 +1,4 @@
-var employees = [{
-    name: "",
-    email: "",
-    phone: "",
-    address: "",
-    pSchool: "",
-    hSchool: "",
-    college: "",
-}];
-
-var provinces = [
+var states = [
     "Guayas",
     "Manabi",
     "Pichincha",
@@ -26,14 +16,45 @@ var provinces = [
     "Napo",
     "Orellana"
 ];
-
+//Event listener that loads the states to the select element
 document.addEventListener("DOMContentLoaded", function() {
     var select = document.getElementById("inputState");
     select.innerHTML = '<option selected>...</option>';
-    provinces.forEach(function(province) {
-        var option = document.createElement("option");
-        option.value = province;
-        option.text = province;
-        select.appendChild(option);
-    });
+        states.forEach(function(state) {
+            var option = document.createElement("option");
+            option.value = state;
+            option.text = state;
+            select.appendChild(option);
+        });
+});
+//Event listener for the submit button
+document.getElementById("submitButton").addEventListener("click", function() {
+    var employees = [{
+        name: document.getElementById("inputName").value,
+        email: document.getElementById("inputEmail").value,
+        phone: document.getElementById("inputPhone").value,
+        address: document.getElementById("inputAddress").value,
+        province: document.getElementById("inputState").value,
+        zipcode: document.getElementById("inputZip").value,
+        pSchool: document.getElementById("inputPSchool").value,
+        hSchool: document.getElementById("inputHSchool").value,
+        college: document.getElementById("inputCollege").value,
+        degreeTitle: document.getElementById("inputDegreeTitle").value,
+        jobPosition: document.getElementById("inputJobPosition").value,
+        experience: document.getElementById("inputExperience").value,
+        spouse: document.getElementById("inputSpouse").value,
+        childrenQ: document.getElementById("inputChildrenQ").value
+    }];
+
+    for (var i = 0; i < employees.length; i++){
+        console.log(employees[i]);
+    }
+
+    document.querySelectorAll('input').forEach(input => {
+        input.value = "";
+    })
+
+    document.querySelectorAll('select').forEach(select => {
+        select.value = "...";
+    })
 });
