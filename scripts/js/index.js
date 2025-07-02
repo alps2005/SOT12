c8904f190224386e9051f14056a772cc74478fc9
Adrian Palma <adrianpalmasantana@gmail.com>
2025-07-02 08:46:20 -0500

    new validation for empty inputs and select. new alert to confirm the data added to the string. new alert to alert about empty form.

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -29,6 +29,24 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 //Event listener for the submit button
 document.getElementById("submitButton").addEventListener("click", function() {
+    //Check if there are empty inputs or invalid selects
+    let emptyInput = false;
+    let invalidSelect = false;
+    document.querySelectorAll('input').forEach(input => {
+        if (input.value.trim() === ""){
+            emptyInput = true;
+        }
+    });
+    document.querySelectorAll('select').forEach(select => {
+        if (select.value === "..."){
+            invalidSelect = true;
+        }
+    })
+    if (emptyInput || invalidSelect){
+        alert("Existen campos vacios o invalidos!");
+        return;
+    }
+    //If there is no empty stuff add them to the employees array
     var employees = [{
         name: document.getElementById("inputName").value,
         email: document.getElementById("inputEmail").value,
@@ -44,17 +62,17 @@ document.getElementById("submitButton").addEventListener("click", function() {
         experience: document.getElementById("inputExperience").value,
         spouse: document.getElementById("inputSpouse").value,
         childrenQ: document.getElementById("inputChildrenQ").value
-    }];
-
+    }]
     for (var i = 0; i < employees.length; i++){
         console.log(employees[i]);
-    }
-
+    };
+    //Clear the form for each input and select
     document.querySelectorAll('input').forEach(input => {
         input.value = "";
-    })
-
+    });
     document.querySelectorAll('select').forEach(select => {
         select.value = "...";
-    })
+    });
+    //Alert to confirm the employee was added successfully
+    alert(`Nuevo empleado agregado: ${employees[0].name}`);
 });
